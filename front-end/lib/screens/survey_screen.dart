749406819c3273c3b4c13957f5ce107e6ccc4d69
Bonggu Kang <rkdbg11752@naver.com>
2023-02-16 18:50:21 +0900

    survey_idea

--- front-end/lib/screens/survey_screen.dart
+++ front-end/lib/screens/survey_screen.dart
@@ -11,6 +11,7 @@ class SurveyScreen extends StatefulWidget {
 class _SurveyScreenState extends State<SurveyScreen> {
   double _val_1 = 10.0;
   double _val_2 = 20.0;
+  bool visibility_1 = true;
   bool visibility_2 = false;
 
   @override
@@ -26,42 +27,56 @@ class _SurveyScreenState extends State<SurveyScreen> {
       body: Center(
           child: Column(
         children: [
-          SfSlider(
-            value: _val_1,
-            min: 0,
-            max: 5,
-            interval: 1,
-            showTicks: true,
-            onChanged: (dynamic value) {
-              var val = value as double;
-              setState(
-                () {
-                  _val_1 = val.round() as double;
-                  if (_val_1 == 2) visibility_2 = true;
-                  print(_val_1);
+          AnimatedOpacity(
+            opacity: visibility_1 ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+            child: Visibility(
+              visible: visibility_1,
+              child: SfSlider(
+                value: _val_1,
+                min: 0,
+                max: 5,
+                interval: 1,
+                showTicks: true,
+                onChangeEnd: (value) => setState(() {
+                  visibility_2 = true;
+                  visibility_1 = false;
+                }),
+                onChanged: (dynamic value) {
+                  var val = value as double;
+                  setState(
+                    () {
+                      _val_1 = val.round() as double;
+                      print(_val_1);
+                    },
+                  );
                 },
-              );
-            },
+              ),
+            ),
           ),
-          Visibility(
-            visible: visibility_2,
-            child: SfSlider(
-              activeColor: Colors.red,
-              inactiveColor: Colors.red.withOpacity(0.2),
-              value: _val_2,
-              min: 0,
-              max: 5,
-              interval: 1,
-              showTicks: true,
-              onChanged: (dynamic value) {
-                var val = value as double;
-                setState(
-                  () {
-                    _val_2 = val.round() as double;
-                    print(_val_2);
-                  },
-                );
-              },
+          AnimatedOpacity(
+            opacity: visibility_2 ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+            child: Visibility(
+              visible: visibility_2,
+              child: SfSlider(
+                activeColor: Colors.red,
+                inactiveColor: Colors.red.withOpacity(0.2),
+                value: _val_2,
+                min: 0,
+                max: 5,
+                interval: 1,
+                showTicks: true,
+                onChanged: (dynamic value) {
+                  var val = value as double;
+                  setState(
+                    () {
+                      _val_2 = val.round() as double;
+                      print(_val_2);
+                    },
+                  );
+                },
+              ),
             ),
           ),
         ],
