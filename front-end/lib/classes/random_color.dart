1b4e9c451c2ce4a0c608baa0d20aca3ffca43a4e
Bonggu Kang <rkdbg11752@naver.com>
2023-05-24 20:22:36 +0900

    del old

--- front-end/lib/classes/random_color.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-import 'dart:ui';
-
-Color randomColor() {
-  var r = Random().nextInt(255);
-  var g = Random().nextInt(255);
-  var b = Random().nextInt(255);
-  return Color.fromRGBO(r, g, b, 1);
-}
